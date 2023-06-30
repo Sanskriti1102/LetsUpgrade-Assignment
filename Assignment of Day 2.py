17cb6cf29272cdd3b86a1d11ecabccf84f96a9dd
Sanskriti Kadam <111518577+Sanskriti1102@users.noreply.github.com>
2023-06-30 20:06:21 +0530

    Delete Assignment of Day 2.py

--- Assignment of Day 2.py
+++
@@ -1,8 +0,0 @@
-age=int(input("Enter your age: "))
-
-if age<18:
-    print("You are a minor (Age is: {})".format(age))
-elif age >=18 and age<65:
-    print("You are an adult (Age is: {})".format(age))
-elif age>=65:
-    print("You are a senior (Age is: {})".format(age))
