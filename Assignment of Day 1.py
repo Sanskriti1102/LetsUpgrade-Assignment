05b45dc5356f93356729e2ad5aeb2265c9a9621e
Sanskriti Kadam <111518577+Sanskriti1102@users.noreply.github.com>
2023-07-01 10:30:09 +0530

    Commited now

---
+++ Assignment of Day 1.py
@@ -0,0 +1,25 @@
+
+#Problem Statement :
+
+#Guess the output for the following by referring to the below code -
+
+a=int(input("Rate LetsUpgrade from 0 to 10 : "))
+b=input("Enter your review for your live sessions : ")
+name= input("Enter your name : ")
+print("\n")
+print(f"{name} was present for python live session for day 1.")
+print(f"Review: {b}")
+print(f"Rating: {a}")
+
+'''
+Output :
+Rate LetsUpgrade from 0 to 10: 8
+Enter your review for your live sessions: The live sessions were informative and engaging.
+Enter your name : Sanskriti Kadam
+
+
+Sanskriti Kadam was present for python live session for day 1.
+Review: The live sessions were informative and engaging.
+Rating: 8
+'''
+
